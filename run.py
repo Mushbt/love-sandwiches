5e9d720a9467aaaafd74fcb356040a27d547e272
Mushbt <mustafa_habet@hotmail.com>
2023-02-11 15:38:34 +0000

    Update code on py file

--- run.py
+++ run.py
@@ -26,11 +26,13 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
+
         sales_data = data_str.split(",")
-        
+
         if validate_data(sales_data):
             print("Data is valid!")
             break
+
     return sales_data
 
 
@@ -40,7 +42,6 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
-
     try:
         [int(value) for value in values]
         if len(values) != 6:
@@ -53,14 +54,16 @@ def validate_data(values):
 
     return True
 
+
 def update_sales_worksheet(data):
     """
-    Update sales worksheet, add new row with the list provided.
+    Update sales worksheet, add new row with the list data provided
     """
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
+
 
 data = get_sales_data()
 sales_data = [int(num) for num in data]
