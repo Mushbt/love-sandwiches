691b4e3a20c7466e0c28550c4a311738ed25bf0e
Mushbt <mustafa_habet@hotmail.com>
2023-02-11 16:02:40 +0000

    Update code in py file

--- run.py
+++ run.py
@@ -69,15 +69,16 @@ def update_sales_worksheet(data):
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
-
+    
     The surplus is defined as the sales figure subtracted from the stock:
-    - Positive surplus indicates waste.
+    - Positive surplus indicates waste
     - Negative surplus indicates extra made when stock was sold out.
     """
-    print("Calculating surplus date...\n")
+    print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
     print(stock_row)
+
 
 def main():
     """
@@ -86,7 +87,7 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
